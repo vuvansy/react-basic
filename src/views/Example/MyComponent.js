be88682027989f1175a44151a94f4ecc2d8fcda5
vuvansy <vuvansy3001@gmail.com>
2024-04-12 00:20:19 +0700

    #5 State - Kiểm Soát Trạng Thái Ứng Dụng React.JS

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,8 +1,43 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    /**
+     *JSX ==> return block (JSX sẽ trả về một khối - 1 phần tử của HTML)
+     *Khi đặt class cho một phần tử
+     *thay vì khai báo class thì trong React => className
+     *
+     *Tìm hiểu: fragment
+     */
+
+    //State
+    //key:value
+    state = {
+        name: "T.Break",
+        facebook: "Vũ Văn Sỹ",
+    };
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value,
+        });
+    };
+
     render() {
-        return <div>Hello my component </div>;
+        return (
+            <>
+                <div className="first">
+                    <input
+                        value={this.state.name}
+                        type="text"
+                        onChange={(event) => {
+                            this.handleOnChangeName(event);
+                        }}
+                    />
+                    <br></br>
+                    My name is: {this.state.name}
+                </div>
+                <div>My Facebook chanel: {this.state.facebook}</div>
+            </>
+        );
     }
 }
 
